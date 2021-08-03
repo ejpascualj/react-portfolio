33a8f3200ed908616a702da2ccbf707b985b840c
Emilio P <ejpascualj@gmail.com>
2021-08-02 23:57:33 -0500

    added code to Contact.js

--- src/components/Contact.js
+++ src/components/Contact.js
@@ -2,8 +2,18 @@ import React from 'react';
 
 function Contact() {
     return(
-        
+        <div class="container p-1 pt-4" id="contact">
+            <h2 class="text-center">Contact Information</h2>
+            <p>Let's connect! Feel free to reach out in any of the following ways:</p>
+            <ul>
+                <li>Email: <a href="mailto:ejpascualj@gmail.com">ejpascualj@gmail.com</a></li>
+                <li>Phone: (+507) 6025-4183</li>
+                <li>LinkedIn: <a href="https://linkedin.com/in/ejpascualj"
+                        target="_blank">linkedin.com/in/ejpascualj</a></li>
+                <li>GitHub: <a href="https://github.com/ejpascualj" target="_blank">@ejpascualj</a></li>
+            </ul>
+        </div>
     );
 }
 
-export default Contact
+export default Contact;
