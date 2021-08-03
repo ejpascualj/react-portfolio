cd2c99eea135e8ed9c0468b33a9e3b5cdd5fb2c5
Emilio P <ejpascualj@gmail.com>
2021-08-03 00:27:37 -0500

    added routes to App.js

--- src/App.js
+++ src/App.js
@@ -1,5 +1,11 @@
-import logo from './logo.svg';
+import React from 'react';
 import './App.css';
+import Header from './components/Header';
+import Home from './components/Home';
+import Portfolio from './components/Portfolio';
+import Resume from './components/Resume';
+import Contact from './components/Contact';
+import Footer from './components/Footer';
 
 function App() {
   return (
