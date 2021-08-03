0c159b7e828fbf9f99e75c7fcdae6ffffd037e57
Emilio P <ejpascualj@gmail.com>
2021-08-03 18:40:03 -0500

    added Router to App.js

--- src/App.js
+++ src/App.js
@@ -1,22 +1,24 @@
-import React from 'react';
-import { BrowserRouter as Router } from 'react-router-dom';
-import Header from './components/Header';
-import Home from './components/Home';
-import Portfolio from './components/Portfolio';
-import Resume from './components/Resume';
-import Contact from './components/Contact';
-import Footer from './components/Footer';
-import './App.css';
+import React from "react";
+import { BrowserRouter as Router } from "react-router-dom";
+import Header from "./components/Header";
+import Home from "./components/Home";
+import Portfolio from "./components/Portfolio";
+import Resume from "./components/Resume";
+import Contact from "./components/Contact";
+import Footer from "./components/Footer";
+import "./App.css";
 
 function App() {
   return (
     <div className="App">
-      <Header />
-      <Home />
-      <Portfolio />
-      <Resume />
-      <Contact />
-      <Footer />   
+      <Router>
+        <Header />
+        <Home />
+        <Portfolio />
+        <Resume />
+        <Contact />
+        <Footer />
+      </Router>
     </div>
   );
 }
