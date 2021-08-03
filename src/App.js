56a4556c70a5943b8a93c1cf5e98397a277ff38c
Emilio P <ejpascualj@gmail.com>
2021-08-03 00:43:44 -0500

    changed App.js

--- src/App.js
+++ src/App.js
@@ -1,22 +1,23 @@
 import React from 'react';
-import './App.css';
+import { BrowserRouter as Router } from 'react-router-dom';
 import Header from './components/Header';
 import Home from './components/Home';
 import Portfolio from './components/Portfolio';
 import Resume from './components/Resume';
 import Contact from './components/Contact';
 import Footer from './components/Footer';
+import './App.css';
 
 function App() {
   return (
-    <>
+    <div className="App">
       <Header/>
       <Home/>
       <Portfolio/>
       <Resume/>
       <Contact/>
       <Footer/>   
-    </>
+    </div>
   );
     /*<div className="App">
       <header className="App-header">
