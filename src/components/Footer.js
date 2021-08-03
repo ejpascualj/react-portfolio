78ddc871981b79a2d7a290f6745f8fc8f12c9423
Emilio P <ejpascualj@gmail.com>
2021-08-02 23:56:01 -0500

    added code to footer

---
+++ src/components/Footer.js
@@ -0,0 +1,11 @@
+import React from 'react';
+
+function Footer() {
+    return(
+        <footer>
+            <p class="text-center">EJP &copy; 2021</p>
+        </footer>
+    );
+}
+
+export default Footer;
